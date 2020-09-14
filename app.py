514746c972df5ef72a74697421b38689e07d407d
Tate Theisen <tatetheisen@gmail.com>
2020-09-14 12:33:22 -0400

    add sqlite and create table with insert and select queries

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from database import create_table,add_entry, get_entries
+
 menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
@@ -7,20 +9,27 @@ Your selection: """
 
 welcome = "Welcome to the programming diary!"
 
-# user_input = input(menu)
-# while user_input != "3":
-#     user_input = input(menu)
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+    
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+            print(f"{entry[1]}\n{entry[0]}\n\n")
 
 
 print(welcome)
-
+create_table()
 
 # The walrus operator, :=, can be read as "being equal.". 
 # So the while loop means "while user_input, being equal to the user's input, is not equal to 3".
+
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        print("Adding...")
+        prompt_new_entry()
     elif user_input == "2":
-        print("Viewing...")
+        view_entries(get_entries())
     else:
         print("Invalid option, please try again!")
--- database.py
+++ database.py
@@ -1,7 +1,24 @@
-entries = []
+import sqlite3
 
-def add_entry():
-    pass
+connection = sqlite3.connect("data.db")
 
-def view_entries():
-    pass
+def create_table():
+    with connection:
+        connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
+
+
+
+def add_entry(entry_content, entry_date):
+    with connection:
+        connection.execute(
+            "INSERT INTO entries (content, date) VALUES (?, ?);", (entry_content, entry_date)
+        )
+
+
+def get_entries():
+    # cursor = connection.cursor()
+    # cursor.execure("SELECT * FROM entries;")
+
+    # SHORT FORM FOR ^^^
+    cursor = connection.execute("SELECT * FROM entries;")
+    return cursor
